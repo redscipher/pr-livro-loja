ef58a3126791eb58218bf5eb9ec8d709192611d8
redssy <redlemagic@hotmail.com>
2025-04-19 17:32:13 -0300

    feat: servindo app na vercel

--- prlivroloja/settings.py
+++ prlivroloja/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "prlivroloja.wsgi.application"
+WSGI_APPLICATION = "prlivroloja.wsgi.app"
 
 STATIC_URL = BASE_DIR / 'staticfiles'
 
@@ -157,4 +157,3 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
-
