7ca8e623804c919e8c8855141c3a7522ff3b783d
redssy <redlemagic@hotmail.com>
2025-04-19 20:21:58 -0300

    feat: vercel deploy

--- prlivroloja/settings.py
+++ prlivroloja/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -148,6 +145,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # SECURITY WARNING: keep the secret key used in production secret!
